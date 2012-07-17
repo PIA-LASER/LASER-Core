381137bb896e108bfd83f3ee29cffbac5260bc66
Dominik Huebner <contact@dhuebner.com>
2012-07-17 23:08:50 +0200

    meh

--- src/main/java/LASER/App.java
+++ src/main/java/LASER/App.java
@@ -100,7 +100,7 @@ public class App extends Configured implements Tool {
         //Map categories to items
 
         boolean success;
-               /*
+
         Job mapItemMapping = HadoopUtil.buildJob(
                 inputPath,
                 catItemMap,
@@ -330,7 +330,7 @@ public class App extends Configured implements Tool {
             HDFSUtil.cleanupTemporaryPath(conf);
             return -1;
         }
-                 /*
+
         Job debugRecommendationJob = HadoopUtil.buildJob(
                 recommendPrepPairs,
                 debugOutputPath,
@@ -374,7 +374,7 @@ public class App extends Configured implements Tool {
             HDFSUtil.cleanupTemporaryPath(conf);
             return -1;
         }
-            */
+
         return 0;
     }
 }
